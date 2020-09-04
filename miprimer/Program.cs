01ae5104fabbc538c38b3204f90e2dea2f548839
Danny <dannyconcepcion01@gmail.com>
2020-09-03 23:25:36 -0400

    Agregar archivos de proyecto.

---
+++ miprimer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miprimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+            //Este programa te salud
+
+
+        {
+
+            string nombre;
+           
+
+
+            Console.WriteLine("Digite su nombre ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Saludo Mi Nombre es : "+ nombre );
+            Console.ReadLine();
+        }
+    }
+}
